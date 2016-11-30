7c8d3d05988ffbde34ed7a69bda5bf6e76c1ff45
josue pacheco <pca.josue@gmail.com>
2016-11-30 14:02:36 -0500

    responder con json

--- miapp/app.js
+++ miapp/app.js
@@ -1,13 +1,31 @@
+//Localhost:5050/TecnologiasWeb?
+//nombre=josue&cedula=17267991608
+
 var express = require('express');
 var app = express();
 
 app.get('/TecnologiasWeb', function (req, res) {
-  res.send('Hola Mundo');
+  
+    res.send('Hola Mundo');
 });
 
 app.post('/TecnologiasWeb', function (req, res) {
-  res.send('Hola mundo Con Post');
+  
+    //request=> req
+  //response => res
+    var usuario={
+        nombre:'Adrian',
+        cedula:'00000000'
+    };
+    console.log('Lo que tengo en el requeste es: ');
+    console.log(req.headers);
+    console.log('Lo que tengo en el response es: ');
+    console.log(res.headers);
+    //res.send('Hola mundo Con Post');
+    res.json(usuario);
+    
 });
+
 app.put('/TecnologiasWeb', function (req, res) {
   res.send('Hola mundo con put');
 });
