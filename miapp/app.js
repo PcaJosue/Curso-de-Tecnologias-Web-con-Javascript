a944b5e46e965980302ea4f7188c68d2b42f3ea7
josue pacheco <pca.josue@gmail.com>
2016-11-30 13:40:31 -0500

    mas metodos http

--- miapp/app.js
+++ miapp/app.js
@@ -5,6 +5,13 @@ app.get('/TecnologiasWeb', function (req, res) {
   res.send('Hola Mundo');
 });
 
+app.post('/TecnologiasWeb', function (req, res) {
+  res.send('Hola mundo Con Post');
+});
+app.put('/TecnologiasWeb', function (req, res) {
+  res.send('Hola mundo con put');
+});
 app.listen(5050, function () {
   console.log('hola mundo');
 });
+
