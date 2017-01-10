47d4abd0d42d8659722430fe0ab906d066acdf2d
pca.josue <pca.josue@gmail.com>
2017-01-10 13:14:32 -0500

    clases

--- mascotas/api/models/Administrador.js
+++ mascotas/api/models/Administrador.js
@@ -7,25 +7,33 @@
 
 module.exports = {
 
-  attributes:{
-        nombre: {
-            type: 'string',
-            minLength: 5
-        },
-        apellido:{
-            type: 'string'
-        },
-        correo:{
-            type:'string',
-            email: true,
-            defaultsTo: 'correo@invalido.com'
-        },
-        tipo:{
-        type : 'string',
-            enum: ['Perfil','Usuario','Correo'],
-            defaultsTo: 'Usuario'
-    }
-        
-}
+  attributes: {
+      nombres:{
+          type:'string',
+          minLength:5
+      },
+      apellidos:{
+          type:'string',
+          minLength:5
+      },
+      correo:{
+          type:'string',
+          email:true,
+          dafaultsTo:'correo@invalido.com'
+      },
+      tipo:{
+          type:'string',
+          enum:['Perfil','Usuarios','Correos'],
+          dafaultsTo:'Usuarios'
+      }
+
+  }
 };
+
+
+
+
+
+
+
 
--- mascotas/api/models/Usuario.js
+++ mascotas/api/models/Usuario.js
@@ -7,21 +7,31 @@
 
 module.exports = {
 
-  attributes:{
-        nombre: {
-            type: 'string',
-            minLength: 5
-        },
-        apellido:{
-            type: 'string'
-        },
-        correo:{
-            type:'string',
-            email: true,
-            defaultsTo: 'correo@invalido.com'
-        }
-        
-        
-}
+  attributes: {
+      nombres:{
+          type:'string',
+          minLength:5
+      },
+      apellidos:{
+          type:'string',
+          minLength:5
+      },
+      correo:{
+          type:'string',
+          email:true,
+          defaultsTo:'correo@invalido.com'
+      }
+  }
+    
 };
+
+
+
+
+
+
+
+
+
+
 
